86a7833b33b87747fc030461554785de6e7de1ec
egorca_05 <gapeevegor@gmail.com>
2022-03-18 00:51:08 +0300

    Подготовка проекта, создание базовых элементов

--- HyperSpace/WindowFolder/NoRoleFolder/AutorizationWindow.xaml.cs
+++ HyperSpace/WindowFolder/NoRoleFolder/AutorizationWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace HyperSpace
+namespace HyperSpace.WindowFolder.NoRoleFolder
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для AutorizationWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AutorizationWindow : Window
     {
-        public MainWindow()
+        public AutorizationWindow()
         {
             InitializeComponent();
         }
