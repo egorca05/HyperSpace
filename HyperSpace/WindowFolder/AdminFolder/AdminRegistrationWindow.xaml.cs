3b17f7e90b55bd4c8e34fa1b0d06c052acfdc3fb
egorca_05 <gapeevegor@gmail.com>
2022-03-22 20:26:16 +0300

    визуал окна регистрации

---
+++ HyperSpace/WindowFolder/AdminFolder/AdminRegistrationWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HyperSpace.WindowFolder.AdminFolder
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminRegistrationWindow.xaml
+    /// </summary>
+    public partial class AdminRegistrationWindow : Window
+    {
+        public AdminRegistrationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RegistrationBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
