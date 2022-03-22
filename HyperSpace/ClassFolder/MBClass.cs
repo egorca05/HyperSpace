005f6cfd87b48fa28e7f192db74cec8b6b7de0aa
egorca_05 <gapeevegor@gmail.com>
2022-03-22 19:46:37 +0300

    ClassMB

--- HyperSpace/ClassFolder/MBClass.cs
+++ HyperSpace/ClassFolder/MBClass.cs
@@ -10,5 +10,36 @@ namespace HyperSpace.ClassFolder
     internal class MBClass
     {
 
+        public static void MBError(Exception text)
+        { 
+            MessageBox.Show(text.Message, "Ошибка", MessageBoxButton.OK,
+            MessageBoxImage.Error);
+        }
+
+        public static void MBError(string text)
+        {
+            MessageBox.Show(text, "Ошибка", MessageBoxButton.OK,
+                MessageBoxImage.Error);
+        }
+        public static void MBInformation(string textMessage)
+        {
+            MessageBox.Show(textMessage, "Информация",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        public static bool QuestionMessage(string text)
+        {
+            return MessageBoxResult.Yes == MessageBox.Show(text, "Вопрос",
+                MessageBoxButton.YesNo, MessageBoxImage.Question);
+        }
+        public static void ExitMessageBox()
+        {
+            bool resultMB = QuestionMessage("Вы действительно желаете выйти?");
+            if (resultMB == true)
+            {
+                App.Current.Shutdown();
+            }
+        }
+
+
     }
 }
